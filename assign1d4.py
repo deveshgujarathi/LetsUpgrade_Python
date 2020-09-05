d1d9d712f292109948f5528bf32bb403a2ee1228
Devesh Gujarathi <70016314+deveshgujarathi@users.noreply.github.com>
2020-09-05 22:41:28 +0530

    Day 4 Assignment

---
+++ assign1d4.py
@@ -0,0 +1,15 @@
+num1 = 1042000
+num2 = 702648265
+
+for num in range(num1, num2 + 1):
+    order = len(str(num))
+    total = 0
+    temp = num
+    while temp > 0:
+        digit = temp % 10
+        total += digit ** order
+        temp //= 10
+
+    if num == total:
+        print(num)
+        break
